df47c0d30b08b0b25a77e684972bcc4c83aca1b6
Thomas McNiven <hello@vevix.net>
2017-05-07 18:30:41 +1000

    Improve meta tags for posts/comments

--- app/routes/comments.js
+++ app/routes/comments.js
@@ -3,6 +3,7 @@ import get from 'ember-metal/get';
 import set from 'ember-metal/set';
 import service from 'ember-service/inject';
 import DataErrorMixin from 'client/mixins/routes/data-error';
+import clip from 'clip';
 
 export default Route.extend(DataErrorMixin, {
   i18n: service(),
@@ -16,6 +17,7 @@ export default Route.extend(DataErrorMixin, {
 
   afterModel(model) {
     set(this, 'breadcrumb', `Comment by ${get(model, 'user.name')}`);
+    this.setHeadTags(model);
   },
 
   setupController(controller, model) {
@@ -29,5 +31,66 @@ export default Route.extend(DataErrorMixin, {
   titleToken(model) {
     const commenter = get(model, 'user.name');
     return get(this, 'i18n').t('titles.comments', { user: commenter });
+  },
+
+  setHeadTags(model) {
+    const desc = clip(get(model, 'content'), 400);
+    const data = [{
+      type: 'meta',
+      tagId: 'meta-description',
+      attrs: {
+        name: 'description',
+        content: desc
+      }
+    }, {
+      type: 'meta',
+      tagId: 'meta-og-description',
+      attrs: {
+        property: 'og:description',
+        content: desc
+      }
+    }, {
+      type: 'meta',
+      tagId: 'meta-og-image',
+      attrs: {
+        property: 'og:image',
+        content: get(model, 'user.avatar.medium')
+      }
+    }];
+    if (get(model, 'likesCount')) {
+      data.push({
+        type: 'meta',
+        tagId: 'meta-twitter-label1',
+        attrs: {
+          property: 'twitter:label1',
+          content: 'Likes'
+        }
+      }, {
+        type: 'meta',
+        tagId: 'meta-twitter-data1',
+        attrs: {
+          property: 'twitter:data1',
+          content: `${get(model, 'likesCount')}`
+        }
+      });
+    }
+    if (get(model, 'repliesCount')) {
+      data.push({
+        type: 'meta',
+        tagId: 'meta-twitter-label2',
+        attrs: {
+          property: 'twitter:label2',
+          content: 'Replies'
+        }
+      }, {
+        type: 'meta',
+        tagId: 'meta-twitter-data2',
+        attrs: {
+          property: 'twitter:data2',
+          content: get(model, 'repliesCount')
+        }
+      });
+    }
+    return data;
   }
 });
--- app/routes/posts.js
+++ app/routes/posts.js
@@ -3,6 +3,7 @@ import get from 'ember-metal/get';
 import set from 'ember-metal/set';
 import service from 'ember-service/inject';
 import DataErrorMixin from 'client/mixins/routes/data-error';
+import clip from 'clip';
 
 export default Route.extend(DataErrorMixin, {
   i18n: service(),
@@ -17,6 +18,7 @@ export default Route.extend(DataErrorMixin, {
 
   afterModel(model) {
     set(this, 'breadcrumb', `Post by ${get(model, 'user.name')}`);
+    this.setHeadTags(model);
     get(this, 'metrics').invoke('trackImpression', 'Stream', {
       content_list: [`Post:${get(model, 'id')}`],
       location: get(this, 'routeName')
@@ -27,5 +29,66 @@ export default Route.extend(DataErrorMixin, {
     const model = this.modelFor('posts');
     const name = get(model, 'user.name');
     return get(this, 'i18n').t('titles.posts', { user: name });
+  },
+
+  setHeadTags(model) {
+    const desc = clip(get(model, 'content'), 400);
+    const data = [{
+      type: 'meta',
+      tagId: 'meta-description',
+      attrs: {
+        name: 'description',
+        content: desc
+      }
+    }, {
+      type: 'meta',
+      tagId: 'meta-og-description',
+      attrs: {
+        property: 'og:description',
+        content: desc
+      }
+    }, {
+      type: 'meta',
+      tagId: 'meta-og-image',
+      attrs: {
+        property: 'og:image',
+        content: get(model, 'user.avatar.medium')
+      }
+    }];
+    if (get(model, 'postLikesCount')) {
+      data.push({
+        type: 'meta',
+        tagId: 'meta-twitter-label1',
+        attrs: {
+          property: 'twitter:label1',
+          content: 'Likes'
+        }
+      }, {
+        type: 'meta',
+        tagId: 'meta-twitter-data1',
+        attrs: {
+          property: 'twitter:data1',
+          content: `${get(model, 'postLikesCount')}`
+        }
+      });
+    }
+    if (get(model, 'commentsCount')) {
+      data.push({
+        type: 'meta',
+        tagId: 'meta-twitter-label2',
+        attrs: {
+          property: 'twitter:label2',
+          content: 'Comments'
+        }
+      }, {
+        type: 'meta',
+        tagId: 'meta-twitter-data2',
+        attrs: {
+          property: 'twitter:data2',
+          content: get(model, 'commentsCount')
+        }
+      });
+    }
+    return data;
   }
 });
